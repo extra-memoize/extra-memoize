a07190711fb391a2adeb0774db928e4126a94c30
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-18 06:40:56 +0000

    test: add cases

--- __tests__/memoize.spec.ts
+++ __tests__/memoize.spec.ts
@@ -74,43 +74,93 @@ describe(`
       jest.useRealTimers()
     })
 
-    test('sync function', () => {
-      const fn = jest.fn((hello: string) => {
-        jest.setSystemTime(jest.getRealSystemTime() + 200)
-        return 'world'
+    describe('sync function', () => {
+      describe('>= executionTimeThreshold', () => {
+        it('cache', () => {
+          const fn = jest.fn((hello: string) => {
+            jest.setSystemTime(jest.getRealSystemTime() + 200)
+            return 'world'
+          })
+          const map = new Map()
+          const cache = new StringKeyCache(map)
+
+          const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
+          const result1 = memoizedFn('hello')
+          const result2 = memoizedFn('hello')
+
+          expect(result1).toBe('world')
+          expect(result2).toBe('world')
+          expect(fn).toBeCalledTimes(1)
+        })
       })
-      const map = new Map()
-      const cache = new StringKeyCache(map)
 
-      const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
-      const result1 = memoizedFn('hello')
-      const result2 = memoizedFn('hello')
+      describe('< executionTimeThreshold', () => {
+        it('not cache', () => {
+          const fn = jest.fn((hello: string) => {
+            return 'world'
+          })
+          const map = new Map()
+          const cache = new StringKeyCache(map)
 
-      expect(result1).toBe('world')
-      expect(result2).toBe('world')
-      expect(fn).toBeCalledTimes(1)
+          const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
+          const result1 = memoizedFn('hello')
+          const result2 = memoizedFn('hello')
+
+          expect(result1).toBe('world')
+          expect(result2).toBe('world')
+          expect(fn).toBeCalledTimes(2)
+        })
+      })
     })
 
-    test('async function', async () => {
-      const fn = jest.fn(async (hello: string) => {
-        jest.setSystemTime(jest.getRealSystemTime() + 200)
-        return 'world'
+    describe('async function', () => {
+      describe('>= executionTimeThreshold', () => {
+        it('cache', async () => {
+          const fn = jest.fn(async (hello: string) => {
+            jest.setSystemTime(jest.getRealSystemTime() + 200)
+            return 'world'
+          })
+          const map = new Map()
+          const cache = new StringKeyCache(map)
+
+          const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
+          const result1 = memoizedFn('hello')
+          const proResult1 = await result1
+          const result2 = memoizedFn('hello')
+          const proResult2 = await result2
+
+          expect(result1).toBePromise()
+          expect(proResult1).toBe('world')
+          expect(result2).toBePromise()
+          expect(proResult2).toBe('world')
+          expect(result1).not.toBe(result2)
+          expect(fn).toBeCalledTimes(1)
+        })
       })
-      const map = new Map()
-      const cache = new StringKeyCache(map)
 
-      const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
-      const result1 = memoizedFn('hello')
-      const proResult1 = await result1
-      const result2 = memoizedFn('hello')
-      const proResult2 = await result2
+      describe('< executionTimeThreshold', () => {
+        it('not cache', async () => {
+          const fn = jest.fn(async (hello: string) => {
+            jest.setSystemTime(jest.getRealSystemTime() + 200)
+            return 'world'
+          })
+          const map = new Map()
+          const cache = new StringKeyCache(map)
 
-      expect(result1).toBePromise()
-      expect(proResult1).toBe('world')
-      expect(result2).toBePromise()
-      expect(proResult2).toBe('world')
-      expect(result1).not.toBe(result2)
-      expect(fn).toBeCalledTimes(1)
+          const memoizedFn = memoize({ cache, executionTimeThreshold: 200 }, fn)
+          const result1 = memoizedFn('hello')
+          const proResult1 = await result1
+          const result2 = memoizedFn('hello')
+          const proResult2 = await result2
+
+          expect(result1).toBePromise()
+          expect(proResult1).toBe('world')
+          expect(result2).toBePromise()
+          expect(proResult2).toBe('world')
+          expect(result1).not.toBe(result2)
+          expect(fn).toBeCalledTimes(1)
+        })
+      })
     })
   })
 })
